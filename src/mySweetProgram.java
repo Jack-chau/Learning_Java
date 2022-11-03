da4293e6d08351c44797d305c15f2cf2d35adc9f
JackChau <chaukaho149@gmail.com>
2022-11-03 10:37:48 +0800

    second commit

---
+++ src/mySweetProgram.java
@@ -0,0 +1,12 @@
+public class mySweetProgram {
+    public static void main ( String[] args ) {
+        System.out.println( "Hello World" );
+
+        var x = new mySweetProgram( );
+        x.tacos( );
+    }
+
+    public void tacos( ) {
+        System.out.println ( "I love Tacos" );
+    }
+}
