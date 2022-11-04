d3f27830dfc3863725bce3c33479e75d53da5fa5
JackChau <chaukaho149@gmail.com>
2022-11-04 10:12:08 +0800

    to method

--- src/arrguments.java
+++ src/arrguments.java
@@ -1,5 +1,7 @@
 public class arrguments {
     public static void main ( String[] args ) {
-        System.out.println ( "Hello World" );
+        System.out.println ( "Hello World " +  args[0] );
     }
 }
+
+
